28f79f364dd4e3fd7a32157331336c571d130df5
Ingo Bürk <ingo.buerk@tngtech.com>
2019-05-06 13:43:47 -0300

    updated script to run on the test server I created when called with ./run_forefer.sh test

--- run_forever.sh
+++ run_forever.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
+# ./run_forever test
+# will run the bot on a test server
+# this script swaps the values on auth.json so that the "token" key is always set to what we want 
+
+running_on_test_server="false"
+if [ "x$1" != "x" ] && [ $1 = "test" ]; then
+    running_on_test_server="true"
+fi
+
+echo $running_on_test_server
+
+# if grep returns nothing it was running on test server
+if [ "x`grep tokenTest auth.json`" = "x" ]; then
+    was_running_on_test_server="true"
+else
+    was_running_on_test_server="false"
+fi
+
+echo $was_running_on_test_server
+
+# if it wasn't running on test server and we now want that then we need to update the token on auth.json
+if [ $was_running_on_test_server = "false" ] && [ $running_on_test_server = "true" ]; then
+    sed -e 's/\"token\"/\"tokenFusion\"/' -e 's/\"tokenTest\"/\"token\"/' -i auth.json
+fi	
+
+
+# if it wasn't running on fusion server and we now want that then we need to update the token on auth.json
+if [ $was_running_on_test_server = "true" ] && [ $running_on_test_server = "false" ]; then
+    sed -e 's/\"token\"/\"tokenTest\"/' -e 's/\"tokenFusion\"/\"token\"/' -i auth.json
+fi	
+
 while true
 do
     sleep 1
     node bot.js
     echo "[$(date)] bot exited with code $?. restarting ..."
 done
-
