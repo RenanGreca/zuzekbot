aa1eb4db635138346604fbdffd7f6e6279ab42e6
Cainã Trevisan <cainact@gmail.com>
2019-05-09 20:28:55 -0300

    now running update_feedback_quotes.sh will also upload quotes to a pastebin. create_pastebin.js does the main work of formatting quotes.json into a readable output and using the pastebin API

--- update_feedback_quotes.sh
+++ update_feedback_quotes.sh
@@ -1,3 +1,10 @@
-git add feedback.json quotes.json
-git commit -m "updated feedbacks and quotes"
+#!/bin/bash
+
+node create_pastebin.js
+
+# remove byproducts of create_pastebin.js
+rm quotes.txt 
+
+git add feedback.json quotes.json pastebin.json
+git commit -m "updated feedbacks, quotes and pastebin"
 git push origin master
