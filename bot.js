4528b5801648fa92f9daec85bdadc094cc7cb280
Renan Greca <renangreca@gmail.com>
2019-05-21 18:44:22 +0200

    added error message to price

--- bot.js
+++ bot.js
@@ -963,6 +963,8 @@ function getPrice(user, channel, args) {
                     return;
                 }
             });
+            channel.send(defaultEmbed().setDescription("Jogo não encontrado na eShop solicitada."));
+            return;
         });
     } else {
         price.getGameDetails(game_info, curr, function(price_details) {
